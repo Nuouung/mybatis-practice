9db79866e8275dc40d9510df3e0906d3f0dce83c
Jinseok Lee <jin6016@gmail.com>
2022-02-20 17:41:24 +0900

    Initial commit

---
+++ src/test/java/mybatis/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package mybatis.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
